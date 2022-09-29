9e6c3ec9de8eeffe91cd9335ab3f40cef1cb0ed1
Muhammad Talha <fast@nu.edu.pk>
2022-09-29 13:54:29 +0500

    comitted

---
+++ Hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hp on 9/29/2022.
+//
+
+#include "Hello.h"
---
+++ Hello.h
@@ -0,0 +1,16 @@
+//
+// Created by hp on 9/29/2022.
+//
+
+#ifndef SCDREPOSITERY_HELLO_H
+#define SCDREPOSITERY_HELLO_H
+
+
+
+class Hello {
+
+};
+
+
+
+#endif //SCDREPOSITERY_HELLO_H
